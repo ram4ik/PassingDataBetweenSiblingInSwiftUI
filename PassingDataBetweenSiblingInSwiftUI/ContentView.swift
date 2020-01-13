180dafa8ffeaa23719da5095728f5c5d81c443e4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-13 11:09:42 +0200

    Initial Commit

---
+++ PassingDataBetweenSiblingInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PassingDataBetweenSiblingInSwiftUI
+//
+//  Created by Ramill Ibragimov on 13.01.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
